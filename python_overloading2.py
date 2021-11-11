1fe3ae1852ee9a72c77ccbaea03ff2726050d036
Dibyajyoti rath <dibyajyotirath2@gmail.com>
2021-11-11 19:24:50 +0530

    new for practice

---
+++ python_overloading2.py
@@ -0,0 +1,10 @@
+class a:
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+    def __gt__(self,other):
+        return self.marks > self.other
+    
+a1=a("dibya",100)
+b=a("ravi",80)
+print(a>b)
---
+++ python_overloading3.py
@@ -0,0 +1,27 @@
+class student:
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+    def __gt__(self,other):
+        return self.marks> other.marks
+    def __le__(self,other):
+        return self.marks<=other.marks
+
+s=student("dibya",100)
+s1=student("ravi",80)
+print(s>s1)
+print(s<s1)
+
+print("*************************************************")
+
+class dibya:
+    def __init__(self,roll,marks):
+        self.roll=roll
+        self.marks=marks
+    def __ipow__(self,other):
+        return self.roll**other.marks
+    def __imod__(self,other):
+        return self.roll% other.marks
+d=dibya(2000,100)
+d2=dibya(3000,200)
+print(d**d2)
